962a20704f043bdc0488d31f456dc7ebd3b12082
swynnejr <saucersam@gmail.com>
2021-06-28 16:49:01 -0500

    p1 & p2 complete

---
+++ script.js
@@ -0,0 +1,14 @@
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var p1 = pizzaOven("deep dish", "traditional", ["provolone"], ["pepperoni", "sausage"]);
+console.log(p1);
+
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(p2);
